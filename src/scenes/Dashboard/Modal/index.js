6b0d4c31ec9243a33e9e3fca1ea64afaf7220fcd
Revin Tanri <tanrirevin@gmail.com>
2021-09-18 14:29:01 +0800

    Fix the error in modal input, and set the firebase to the right path

--- src/scenes/Dashboard/Modal/index.js
+++ src/scenes/Dashboard/Modal/index.js
@@ -1,110 +1,74 @@
 import { useState, useEffect } from 'react';
 import { Button, Form, Input } from 'antd';
-import { FormattedText } from 'react-intl'
 import { EditOutlined, PlusOutlined } from "@ant-design/icons";
-import { collection, addDoc } from "firebase/firestore";
+import { doc, updateDoc, arrayUnion } from "firebase/firestore";
 import { FormattedMessage } from 'react-intl'
-import { firebaseApp, db } from '../../../services/firebase/firebase';
+import { db } from '../../../services/firebase/firebase';
 import { Text, H2, Spacer } from '../../../linaria-components';
 
 
 const ModalInput = ({ closeModal }) => {
-  const [modalText, setModalText] = useState("");
+  // const [modalBody, setModalBody] = useState("");
   const [isLoading, setIsLoading] = useState(false)
-  const [mealName, setMealName] = useState("")
-  const [calories, setCalories] = useState(null)
-  const [otherMealName, setOtherMealName] = useState("")
-  const [otherCalories, setOtherCalories] = useState(null)
+  const testCardID = "7ltvd30AQzDymXsptrto"
 
   useEffect(() => {
-    setModalText(modalContent)
-    return () => {
-    }
+    // setModalBody(modalContent(onFinish, setMealName, setCalories, isLoading))
   }, [])
 
-  const testCardID = "7GvhOprgJf3qSHCfCdNw"
-
-  // const onFinish = values => {
-  const handleOK = async () => {
-    setModalText('Saving data...');
-    setIsLoading(true);
-
-    try {
-      const docRef = await addDoc(collection(db, "meals"), {
-        meal_name: mealName,
-        calories: calories,
-        card_id: testCardID
-      });
-      console.log("Record written with ID: ", docRef.id);
-      setIsLoading(false);
-      setModalText('Your input is saved succesfully');
-    }
-    catch (e) {
-      setIsLoading(false);
-      setModalText('Input Failed');
-      console.log("Error adding document: ", e);
-    }
-
-    setTimeout(() => {
-      closeModal()
-    }, 2000)
-  };
-
   const handleCancel = () => {
-    console.log('Clicked cancel button');
     closeModal()
     setIsLoading(false);
   };
 
-  const modalContent = () => {
-    return (
-      <Form className="food-log-form"
-      // initialValues={{ remember: true }} onFinish={onFinish}
-      >
-        <div className="food-log-form__input--wrapper">
-          <Form.Item
-            id="meal-name"
-            name="food"
-            className="form-item-label"
-            rules={[{ required: true, message: "Enter your meal name" }]}
-          >
-            <Text>
-              <FormattedMessage
-                id="modal.input.meal_name"
-                defaultMessage="Meal Name"
-              />
-            </Text>
-            <Input
-              prefix={<EditOutlined className="site-form-item-icon" />}
-              placeholder="e.g. burrito, salad"
-              onChange={e => (setMealName(e.target.value))}
-              className="meal-name-input"
-            />
-          </Form.Item>
 
-          <Form.Item
-            id="calories"
-            name="calories"
-            className="form-item-label"
-            rules={[{ required: true, message: "How much is the calories (in kcal)?" }]}
-          >
-            <Text>
-              <FormattedMessage
-                id="modal.input.calory_intake"
-                defaultMessage="Calory Intake (in kcal)"
-              />
-            </Text>
-            <Input
-              prefix={<PlusOutlined className="site-form-item-icon" />}
-              placeholder="e.g. 250 or 800"
-              onChange={e => (setCalories(e.target.value))}
-              className="calories-input"
-            />
-          </Form.Item>
-        </div>
-      </Form>
+  const CloseButton = () => {
+    return (
+      <Button className="primary-button--full-width" loading={isLoading} onClick={handleCancel}>
+        <FormattedMessage
+          id="modal.button.close"
+          defaultMessage="Close"
+        />
+      </Button>
     )
   }
+
+  // const handleOK = async (props) => {
+  const onFinish = async (values) => {
+    // setModalBody('Saving data...');
+    setIsLoading(true);
+
+
+    console.log("==checking state==")
+    console.log(values)
+
+    try {
+      const cardReference = doc(db, "cards", testCardID);
+      await updateDoc(cardReference, {
+        meals: arrayUnion({
+          name: values.meal_name,
+          calories: values.calories,
+        })
+      })
+
+      setIsLoading(false);
+      // setModalBody(
+      //   <>
+      //     <Text>Your input is saved succesfully'</Text>
+      //     <Spacer spacing={32} />
+      //     <CloseButton />
+      //   </>
+      // );
+
+    }
+    catch (e) {
+      setIsLoading(false);
+      // setModalBody('Input Failed');
+      console.log("Error adding document: ", e);
+    }
+    closeModal()
+  };
+
 
   return (
     <div>
@@ -121,22 +85,140 @@ const ModalInput = ({ closeModal }) => {
           <Spacer spacing={32} />
         </div>
         <div className="modal-body">
-          {modalText}
-        </div>
-        <div className="modal-footer">
-          <Button className="primary-button--full-width" loading={isLoading} onClick={handleOK}>
-            <FormattedMessage
-              id="modal.button.submit"
-              defaultMessage="Save"
-            />
-          </Button>
-        </div>
+          <Form
+            className="food-log-form"
+            initialValues={{ remember: true }}
+            requiredMark="optional"
+            onFinish={onFinish}
+          >
+            <div className="food-log-form__input--wrapper">
+              {/* 1st item */}
+              <Form.Item
+                name="meal_name"
+                rules={[{ required: true, message: "Enter food name" }]}
+              >
+                <Text>
+                  <FormattedMessage
+                    id="modal.input.meal_name"
+                    defaultMessage="Meal Name"
+                  />
+                </Text>
+                <Input
+                  prefix={<EditOutlined className="site-form-item-icon" />}
+                  className="meal-name-input"
+                />
+              </Form.Item>
 
+              {/* 2nd item */}
+              <Form.Item
+                name="calories"
+                rules={[
+                  { required: true, message: "Enter calory intake" },
+                ]}
+              >
+                <Text>
+                  <FormattedMessage
+                    id="modal.input.calory_intake"
+                    defaultMessage="Calory Intake (in kcal)"
+                  />
+                </Text>
+                <Input
+                  prefix={<PlusOutlined className="site-form-item-icon" />}
+                  className="meal-name-input"
+                />
+              </Form.Item>
+            </div>
+
+            {/* submit button */}
+            <div className="submit-button-wrapper">
+              <Form.Item>
+                <Button
+                  htmlType="submit"
+                  className="primary-button--full-width"
+                  loading={isLoading}
+                  disabled={isLoading}
+                >
+                  <FormattedMessage
+                    id="modal.button.submit"
+                    defaultMessage="Submit"
+                  />
+                </Button>
+              </Form.Item>
+            </div>
+          </Form>
+        </div>
       </div>
-
-
     </div>
   );
 };
 
 export default ModalInput
+
+
+
+
+// const modalContent = (onFinish, setMealName, setCalories, isLoading) => {
+//   return (
+//     <Form
+//       className="food-log-form"
+//       initialValues={{ remember: true }}
+//       onFinish={onFinish}
+//     >
+//       <div className="food-log-form__input--wrapper">
+//         <Form.Item
+//           name="meal_name"
+//           className="form-item-label"
+//           rules={[{ required: true, message: "Enter your meal name" }]}
+//         >
+//           <Text>
+//             <FormattedMessage
+//               id="modal.input.meal_name"
+//               defaultMessage="Meal Name"
+//             />
+//           </Text>
+//           <Input
+//             prefix={<EditOutlined className="site-form-item-icon" />}
+//             placeholder="e.g. burrito, salad"
+//             onChange={e => (setMealName(e.target.value))}
+//             className="meal-name-input"
+//           />
+//         </Form.Item>
+
+//         <Form.Item
+//           name="calories"
+//           className="form-item-label"
+//           rules={[{ required: true, message: "How much is the calories (in kcal)?" }]}
+//         >
+          // <Text>
+          //   <FormattedMessage
+          //     id="modal.input.calory_intake"
+          //     defaultMessage="Calory Intake (in kcal)"
+          //   />
+          // </Text>
+//           <Input
+//             prefix={<PlusOutlined className="site-form-item-icon" />}
+//             placeholder="e.g. 250 or 800"
+//             onChange={e => (setCalories(e.target.value))}
+//             className="calories-input"
+//           />
+//         </Form.Item>
+//       </div>
+
+//       <Form.item>
+//         <Button
+//           htmlType="submit"
+//           className="primary-button--full-width"
+//           loading={isLoading}
+//           disabled={isLoading}
+//         >
+          // <FormattedMessage
+          //   id="modal.button.submit"
+          //   defaultMessage="Submit"
+          // />
+//         </Button>
+//       </Form.item>
+
+//     </Form>
+//   )
+// }
+
--- src/scenes/Dashboard/template.js
+++ src/scenes/Dashboard/template.js
@@ -4,7 +4,7 @@ import { Text, H1, H2, Container, Flexbox, Spacer } from '../../linaria-componen
 import { gray_2, red, green, secondary } from '../../linaria-components'
 import { FormattedMessage } from 'react-intl'
 import DailyCard from './DailyCard'
-import { collection, getDocs, where } from "firebase/firestore";
+import { collection, query, where, getDocs } from "firebase/firestore";
 import { db } from '../../services/firebase/firebase';
 
 
@@ -22,72 +22,75 @@ const DashboardPage = () => {
   }, [])
 
 
+
+  // const q = query(collection(db, "cities"), where("capital", "==", true));
+
+  // const querySnapshot = await getDocs(q);
+
+  // querySnapshot.forEach((doc) => {
+  //   // doc.data() is never undefined for query doc snapshots
+  //   console.log(doc.id, " => ", doc.data());
+  // });
+
+
   const getAllCard = async () => {
+    let cardCollection = query(collection(db, "cards"), where("user_id", "==", "CuGTCvX8Y6B7gIKvcfAy"));
 
-    const result = await getDocs(collection(db, "cards"), where("user_id", "==", "CuGTCvX8Y6B7gIKvcfAy"))
-      .then(docResp => {
-        let docArray = [];
-        docResp.forEach((card) => {
-          let total = 0
-          let remain = 0
-          let card_color = COLOR_GREEN.green
+    const cardSnapshot = await getDocs(cardCollection)
+    let docArray = []
+    cardSnapshot.forEach((doc) => {
+      // cards.push(doc.data())
+      let currentCard = doc.data()
+      let total = 0
+      let remain = 0
+      let card_color = COLOR_GREEN.green
 
 
-          if (card.data().meals) {
-            let mealArray = card.data().meals
-            let mealz = []
+      if (currentCard.meals) {
+        let mealArray = currentCard.meals
+        let foodData = []
 
-            if (mealArray[0]) {
+        if (mealArray[0]) {
 
-              for (const key of mealArray) {
-                mealz.push(`${key.name} (${key.calories})`)
-                total += parseInt(key.calories);
-              }
-              if (total >= metabolism_rate) {
-                card_color = COLOR_RED.red
-              }
-              remain = metabolism_rate - total
-              console.log(remain)
-            }
-
-            docArray.push({
-              key: card.id,
-              id: card.id,
-              title: card.data().title,
-              user: card.data().user_id,
-              color: card_color,
-              total: total,
-              remain: remain,
-              meal1: mealz[0],
-              meal2: mealz[1],
-              meal3: mealz[2],
-              meal4: mealz[3],
-              meal5: mealz[4],
-            })
-            return;
+          for (const key of mealArray) {
+            foodData.push(`${key.name} (${key.calories})`)
+            total += parseInt(key.calories);
           }
-
-          docArray.push({
-            key: card.id,
-            id: card.id,
-            title: card.data().title,
-            user: card.data().user_id,
-            color: card_color,
-            total: total,
-            remain: remain,
-          })
+          if (total >= metabolism_rate) {
+            card_color = COLOR_RED.red
+          }
+          remain = metabolism_rate - total
         }
-        )
-        console.log(docArray)
-        return docArray
-      })
-      .catch(err => {
-        console.log(err)
-      })
-    console.log(typeof metabolism_rate)
-    setCardDeck(result)
-  }
 
+        docArray.push({
+          key: currentCard.id,
+          id: currentCard.id,
+          title: currentCard.title,
+          user: currentCard.user_id,
+          color: card_color,
+          total: total,
+          remain: remain,
+          meal1: foodData[0],
+          meal2: foodData[1],
+          meal3: foodData[2],
+          meal4: foodData[3],
+          meal5: foodData[4],
+        })
+        return;
+      }
+
+      docArray.push({
+        key: currentCard.id,
+        id: currentCard.id,
+        title: currentCard.title,
+        user: currentCard.user_id,
+        color: card_color,
+        total: total,
+        remain: remain,
+      })
+    })
+    setCardDeck(docArray)
+  }
 
   return <div>
     <H1 textAlign="center">
